602046c864537b3c87dbb1d58cb3162b0af0d1c7
mdmhriday <mdmhriday@gmail.com>
2024-07-28 05:13:59 +0200

    add color file

---
+++ src/color.h
@@ -0,0 +1,25 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+#include<iostream>
+
+using color = vec3;
+
+void write_color(std::ostream& out, const color& pixel_color) {
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+
+    // translate the [0, 1] component values to the byte range [0, 255]
+    int r_byte = int(255.999 * r);
+    int g_byte = int(255.999 * g);
+    int b_byte = int(255.999 * b);
+
+    // write out pixel color components.
+    out << r_byte << ' ' << g_byte << ' ' << b_byte << "\n";
+
+}
+
+#endif
