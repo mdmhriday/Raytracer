fd8c7e9638e441d83e408d386785eab275580bd6
mdmhriday <mdmhriday@gmail.com>
2024-07-30 03:05:58 +0200

    add utilities file

---
+++ src/utils.h
@@ -0,0 +1,32 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <memory>
+
+// c++ Std using
+
+using std::make_shared;
+using std::shared_ptr;
+
+// constant
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// utilities functions
+inline double degree_to_radians(doubl degree){
+    return degree * pi / 180.0;
+}
+
+// common headers
+
+#include "color.h"
+#include "ray.h"
+#include "vec3.h"
+
+
+
+#endif
