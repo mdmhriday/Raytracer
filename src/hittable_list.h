3014aa20facc034c37d6bd5ce4a48e98747181e4
mdmhriday <mdmhriday@gmail.com>
2024-08-01 13:34:13 +0200

    update class

--- src/hittable_list.h
+++ src/hittable_list.h
@@ -19,22 +19,22 @@ class hittable_list : public hittable{
             objects.push_back(object);
         }
 
-        bool hit(const ray& r, interval ray_t, hit_record& rec) const override{
+        bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
             hit_record temp_rec;
             bool hit_anything = false;
-            auto closest_to_far = ray_t.max;
-
-            for(const auto& object : objects){
-                if(object->hit(r, interval(ray_t.min, closest_to_far), temp_rec)){
+            auto closest_so_far = ray_t.max;
+            
+            for (const auto& object : objects) {
+                if(object->hit(r, interval(ray_t.min, closest_so_far), temp_rec)) {
                     hit_anything = true;
-                    closest_to_far = temp_rec.t;
+                    closest_so_far = temp_rec.t;
                     rec = temp_rec;
+                    }
                 }
-            }
+                
             return hit_anything;
+            
         }
-
-
 
 };
 
--- src/utils.h
+++ src/utils.h
@@ -2,6 +2,7 @@
 #define UTILS_H
 
 #include <cmath>
+#include <cstlib>
 #include <iostream>
 #include <limits>
 #include <memory>
@@ -19,6 +20,14 @@ const double pi = 3.1415926535897932385;
 // utilities functions
 inline double degrees_to_radians(double degrees){
     return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max){
+    return min + (max-min)*random_double();
 }
 
 // common headers
